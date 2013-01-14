8eb0604135beb93e86d3bba51fef8bfb9360b86d
stargazer <already.late@gmail.com>
2013-01-14 13:47:42 +0100

    Restructured

--- setup.py
+++ setup.py
@@ -3,14 +3,9 @@ from setuptools import setup, find_packages
 setup(
 	name = "webserver-monitor",
 	version = "0.1",
-	packages = find_packages('src'),
-	package_dir = {'': 'src'},
-	install_requires = [
+	packages = find_packages(),
+	install_requires=(
 		'setuptools',
 		'psutil',
-	],
-	entry_points = """
-	[console_scripts]
-	monitor = monitor.monitor:main
-	""",
+	),
 )
