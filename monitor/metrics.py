e4aec67158cf5c15f9d65c0773d15a4f0308bebc
stargazer <stargazer@HP.(none)>
2013-01-20 18:51:50 +0100

    Monitors the activity of the list of process names it has been given

--- monitor/metrics.py
+++ monitor/metrics.py
@@ -2,22 +2,25 @@ import threading, psutil
 from time import sleep
  
 class Measurement(threading.Thread):
-    """ This thread, polls the system, saves the observations on list `self.measurements`.
-        This list contains Process instances, as implemented by the `psutil` module.
-
+    """ 
+    This thread, polls the system, saves the observations on list `self.measurements`.
+    This list contains Process instances, as implemented by the `psutil` module.
     """
     INTERVAL = 1.0 
 
-    def __init__(self, measurements, lock):
+    def __init__(self, measurements, lock, monitored_processes):
         """ Creates an instance of a thread, that will poll the system
             and get the measurements.
 
             @param terminate: Event that will signal the thread to terminate
             @param measurements: List where measurements will be stored
             @param lock: Used to control access to `measurements`
+            @param monitored_processes: A list of the names of the processes we want to
+            monitor
         """       
         self.measurements = measurements
         self.lock = lock
+        self.monitored_processes = monitored_processes
 
         super(Measurement, self).__init__()
         self.daemon = True
@@ -38,27 +41,26 @@ class Measurement(threading.Thread):
 
         del self.measurements[:]
         for process in psutil.process_iter():               
-            if process.cmdline:
-                if 'wsgi' in process.cmdline[0] or\
-                   'apache' in process.cmdline[0]:
-                    # Measurements for the process ``process``
-                    measurement = []
-                    try:
-                        measurement.append(process.cmdline[0])
-                        measurement.append(process.pid)
-                        measurement.append(process.ppid)
-                        measurement.append(process.username)
-                        measurement.append(process.get_cpu_percent())
-                        measurement.append(process.get_memory_info()[0] / (1024**2))
-                        measurement.append(process.get_memory_info()[1] / (1024**2))
-                        measurement.append(round(process.get_memory_percent(), 3))
-                        measurement.append(process.get_num_threads())
-                    except:
-                        # happens for example if some measurements needs
-                        # different permissions to be taken
-                        pass
-                    else:
-                        self.measurements.append(measurement)
+            if process.cmdline\
+            and [p for p in self.monitored_processes if p in process.cmdline[0]]:
+                # Measurements for the process ``process``
+                measurement = []
+                try:
+                    measurement.append(process.cmdline[0])
+                    measurement.append(process.pid)
+                    measurement.append(process.ppid)
+                    measurement.append(process.username)
+                    measurement.append(process.get_cpu_percent())
+                    measurement.append(process.get_memory_info()[0] / (1024**2))
+                    measurement.append(process.get_memory_info()[1] / (1024**2))
+                    measurement.append(round(process.get_memory_percent(), 3))
+                    measurement.append(process.get_num_threads())
+                except:
+                    # happens for example if some measurements needs
+                    # different permissions to be taken
+                    pass
+                else:
+                    self.measurements.append(measurement)
         # UNLOCK    
         self.lock.release()
 
